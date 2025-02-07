bfbfeef351b0a2714e6e8a3a793f448a0f317381
Surya558 <159058702+SuryaThanneru@users.noreply.github.com>
2025-02-07 13:01:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+// import React, { Component } from 'react'
+
+// import FBCPropEx from './propsex/FBCPropEx'
+
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <FBCPropEx
+//         username= "Kathi Kushu"
+//         isMarried={true}
+        
+//         hobbies={["Reading ", "eating", "Sleeping"]}
+//         />
+//       </div>
+//     )
+//   }}
+import React from 'react'
+import FunctionalComponent from './components/FunctionalComponent'
+
+const App = () => {
+  return (
+    <div>
+      <FunctionalComponent/>
+    </div>
+  )
+}
+
+export default App
